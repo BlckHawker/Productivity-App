bba535dfda7a1f5330d806d670718beb6cf676aa
Jabrecia Washington <jabwashi2@gmail.com>
2025-09-05 16:16:45 -0400

    sorted imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+/* eslint-disable react/react-in-jsx-scope */
+
 import "./index.css";
 import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
