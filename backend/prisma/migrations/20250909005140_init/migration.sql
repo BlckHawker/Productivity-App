b24c1a076272a4b0385c8e914be9897c7fb863ad
Jabrecia Washington <jabwashi2@gmail.com>
2025-09-08 20:51:57 -0400

    migrate folder :)

---
+++ backend/prisma/migrations/20250909005140_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" SERIAL NOT NULL,
+    "color" VARCHAR(7) NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Section" (
+    "id" SERIAL NOT NULL,
+    "project_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "is_other" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Section_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_name_key" ON "public"."Project"("name");
+
+-- AddForeignKey
+ALTER TABLE "public"."Section" ADD CONSTRAINT "Section_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "public"."Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
