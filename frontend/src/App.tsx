a68cb2f609eee72305184047f5b4e97263edeaa4
Jabrecia Washington <jabwashi2@gmail.com>
2025-09-10 14:34:15 -0400

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,7 @@
  * Root component for the frontend React application.
  * Displays testing creation task code to verify backend support. Does not reflect final file structure
  */
-import { useEffect, useState } from "react";
 import React from "react";
-import type {Task} from "./interfaces.js";
 
 /**
  * Renders the main application UI.
