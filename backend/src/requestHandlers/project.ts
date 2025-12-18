525dc13d0a1fbf42a0569c99b9c3cea0f19f2910
Kovu Jackson-Bentley <62062830+BlckHawker@users.noreply.github.com>
2025-12-18 10:38:24 -0500

    Clarify "update project" swagger documentation (#139)

--- backend/src/requestHandlers/project.ts
+++ backend/src/requestHandlers/project.ts
@@ -18,19 +18,19 @@ import { StatusCode } from "status-code-enum";
  * @returns
  */
 const deleteProjectById = async (req: Request, res: Response) => {
-	const id = Number(req.params.id);
-	const validId = utils.assertArgumentsNumber({ id });
-	if (!validId.success) {
-		return res.status(StatusCode.ClientErrorBadRequest).json(validId);
-	}
+    const id = Number(req.params.id);
+    const validId = utils.assertArgumentsNumber({ id });
+    if (!validId.success) {
+        return res.status(StatusCode.ClientErrorBadRequest).json(validId);
+    }
 
-	const response = await projectController.deleteProjectById(req.prisma)(id);
+    const response = await projectController.deleteProjectById(req.prisma)(id);
 
-	return utils.sanitizeResponse(
-		response,
-		res,
-		`A project with the id "${id}" could not be found.`
-	);
+    return utils.sanitizeResponse(
+        response,
+        res,
+        `A project with the id "${id}" could not be found.`
+    );
 };
 /**
  * @swagger
@@ -103,52 +103,52 @@ const deleteProjectById = async (req: Request, res: Response) => {
  * @returns
  */
 const updateProject = async (req: Request, res: Response) => {
-	const id = typeof req.body.id === "number" ? req.body.id : NaN;
-	const newName = typeof req.body.name === "string" ? req.body.name.trim() : "";
-	const newColor =
-		typeof req.body.color === "string" ? req.body.color.trim() : "";
-	const validId = utils.assertArgumentsNumber({ id });
-	const validName = utils.assertArgumentsString({ newName });
-	const validColor = utils.assertArgumentsString({ newColor });
-	const invalidName = !validName.success;
-	const invalidColor = !validColor.success;
+    const id = typeof req.body.id === "number" ? req.body.id : NaN;
+    const newName = typeof req.body.name === "string" ? req.body.name.trim() : "";
+    const newColor =
+        typeof req.body.color === "string" ? req.body.color.trim() : "";
+    const validId = utils.assertArgumentsNumber({ id });
+    const validName = utils.assertArgumentsString({ newName });
+    const validColor = utils.assertArgumentsString({ newColor });
+    const invalidName = !validName.success;
+    const invalidColor = !validColor.success;
 
-	if (!validId.success) {
-		return res.status(StatusCode.ClientErrorBadRequest).json(validId);
-	}
+    if (!validId.success) {
+        return res.status(StatusCode.ClientErrorBadRequest).json(validId);
+    }
 
-	if (invalidName && invalidColor) {
-		return res
-			.status(StatusCode.ClientErrorBadRequest)
-			.json({ message: `${validName.message}\n${validColor.message}` });
-	}
+    if (invalidName && invalidColor) {
+        return res
+            .status(StatusCode.ClientErrorBadRequest)
+            .json({ message: `${validName.message}\n${validColor.message}` });
+    }
 
-	const response = await projectController.updateProject(req.prisma)(id, {
-		name: invalidName ? null : newName,
-		color: invalidColor ? null : newColor
-	});
+    const response = await projectController.updateProject(req.prisma)(id, {
+        name: invalidName ? null : newName,
+        color: invalidColor ? null : newColor
+    });
 
-	if (response instanceof Error) {
-		const badRequestStr = [
-			"A project with the id",
-			"Cannot update a project with the same values it currently has.",
-			"Updated project name must be different from the current name.",
-			"Updated project color must be different from the current color.",
-			"Updated project color must be different from the current color.",
-			"A project with the name"
-		];
-		if (badRequestStr.some((str) => response.message.includes(str))) {
-			return res.status(StatusCode.ClientErrorBadRequest).json({
-				message: response.message
-			});
-		}
-	}
+    if (response instanceof Error) {
+        const badRequestStr = [
+            "A project with the id",
+            "Cannot update a project with the same values it currently has.",
+            "Updated project name must be different from the current name.",
+            "Updated project color must be different from the current color.",
+            "Updated project color must be different from the current color.",
+            "A project with the name"
+        ];
+        if (badRequestStr.some((str) => response.message.includes(str))) {
+            return res.status(StatusCode.ClientErrorBadRequest).json({
+                message: response.message
+            });
+        }
+    }
 
-	return utils.sanitizeResponse(
-		response,
-		res,
-		"Contact developers if this line appears. updateProject request handler"
-	);
+    return utils.sanitizeResponse(
+        response,
+        res,
+        "Contact developers if this line appears. updateProject request handler"
+    );
 };
 /**
  * @swagger
@@ -157,27 +157,47 @@ const updateProject = async (req: Request, res: Response) => {
  *     summary: Update a project given its id
  *     tags:
  *       - Project
-  *     requestBody:
+ *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             type: object
- *             required:
- *               - id
- *             properties:
- *               id:
- *                 type: number
- *                 description: The id of the project that wants to be updated
- *                 example: 1
- *               name:
- *                 type: string
- *                 description: The new name of the project
- *                 example: "Gym"
- *               color:
- *                 type: string
- *                 description: The new color of the project
- *                 example: "#FF5733"
+ *             oneOf:
+ *               - type: object
+ *                 required: [id, name]
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "NEW NAME"
+ *                 description: Update only the project name
+ *  
+ *               - type: object
+ *                 required: [id, color]
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   color:
+ *                     type: string
+ *                     example: "#FFF"
+ *                 description: Update only the project color
+ *   
+ *               - type: object
+ *                 required: [id, name, color]
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "NEW NAME"
+ *                   color:
+ *                     type: string
+ *                     example: "#FFF"
+ *                 description: Update both the project name and color
  *     responses:
  *       200:
  *         description: The project with its updated values
@@ -191,16 +211,14 @@ const updateProject = async (req: Request, res: Response) => {
  *                   example: 1
  *                 name:
  *                   type: string
- *                   example: Gym
+ *                   example: "Gym"
  *                 color:
  *                   type: string
  *                   example: "#FF5733"
  *                 created_at:
  *                   type: string
- *                   example: "2025-09-07T17:34:03.434Z"
  *                 updated_at:
  *                   type: string
- *                   example: "2025-09-07T17:34:03.434Z"
  *       400:
  *         description: Bad Request
  *         content:
@@ -210,27 +228,8 @@ const updateProject = async (req: Request, res: Response) => {
  *               properties:
  *                 message:
  *                   type: string
- *             examples:
- *               invalid id:
- *                 value:
- *                   message: "Invalid id: must be a valid number"
- *               same color and name:
- *                 value:
- *                   message: "Cannot update a project with the same values it currently has."
- *               project with id doesn't exist:
- *                 value:
- *                   message: "A project with the id \"1\" could not be found."
- *               same name:
- *                 value:
- *                   message: "Updated project name must be different from the current name."
- *               same color:
- *                 value:
- *                   message: "Updated project color must be different from the current color."
-  *               change name to a project that already has that name:
- *                 value:
- *                   message: "A project with the name \"Gym\" already exists"
-
  */
+
 
 /**
  * Get all projects request
@@ -239,8 +238,8 @@ const updateProject = async (req: Request, res: Response) => {
  * @returns
  */
 const getAllProjects = async (req: Request, res: Response) => {
-	const response = await projectController.getAllProjects(req.prisma);
-	return utils.sanitizeResponse(response, res, "No projects were found");
+    const response = await projectController.getAllProjects(req.prisma);
+    return utils.sanitizeResponse(response, res, "No projects were found");
 };
 
 /**
@@ -303,16 +302,16 @@ const getAllProjects = async (req: Request, res: Response) => {
  * @returns
  */
 const getProjectByName = async (req: Request, res: Response) => {
-	const name = String(req.query.name).trim();
-	const validArgs = utils.assertArgumentsString({ name });
-	if (!validArgs.success)
-		return res.status(StatusCode.ClientErrorBadRequest).json(validArgs);
-	const response = await projectController.getProjectByName(req.prisma)(name);
-	return utils.sanitizeResponse(
-		response,
-		res,
-		`A project with the name "${name}" could not be found.`
-	);
+    const name = String(req.query.name).trim();
+    const validArgs = utils.assertArgumentsString({ name });
+    if (!validArgs.success)
+        return res.status(StatusCode.ClientErrorBadRequest).json(validArgs);
+    const response = await projectController.getProjectByName(req.prisma)(name);
+    return utils.sanitizeResponse(
+        response,
+        res,
+        `A project with the name "${name}" could not be found.`
+    );
 };
 /**
  * @swagger
@@ -385,16 +384,16 @@ const getProjectByName = async (req: Request, res: Response) => {
  * @returns
  */
 const getProjectById = async (req: Request, res: Response) => {
-	const id = Number(req.params.id);
-	const validArgs = utils.assertArgumentsNumber({ id });
-	if (!validArgs.success)
-		return res.status(StatusCode.ClientErrorBadRequest).json(validArgs);
-	const response = await projectController.getProjectById(req.prisma)(id);
-	return utils.sanitizeResponse(
-		response,
-		res,
-		`A project with the id "${id}" could not be found.`
-	);
+    const id = Number(req.params.id);
+    const validArgs = utils.assertArgumentsNumber({ id });
+    if (!validArgs.success)
+        return res.status(StatusCode.ClientErrorBadRequest).json(validArgs);
+    const response = await projectController.getProjectById(req.prisma)(id);
+    return utils.sanitizeResponse(
+        response,
+        res,
+        `A project with the id "${id}" could not be found.`
+    );
 };
 /**
  * @swagger
@@ -467,44 +466,44 @@ const getProjectById = async (req: Request, res: Response) => {
  * @returns
  */
 const createProject = async (req: Request, res: Response) => {
-	const name = typeof req.body.name === "string" ? req.body.name.trim() : "";
-	const color = typeof req.body.color === "string" ? req.body.color.trim() : "";
+    const name = typeof req.body.name === "string" ? req.body.name.trim() : "";
+    const color = typeof req.body.color === "string" ? req.body.color.trim() : "";
 
-	const validArgs = utils.mergeResults(
-		utils.assertArgumentsString({ name }),
-		utils.assertArgumentsHexCode({ color })
-	);
-	if (!validArgs.success)
-		return res.status(StatusCode.ClientErrorBadRequest).json(validArgs);
+    const validArgs = utils.mergeResults(
+        utils.assertArgumentsString({ name }),
+        utils.assertArgumentsHexCode({ color })
+    );
+    if (!validArgs.success)
+        return res.status(StatusCode.ClientErrorBadRequest).json(validArgs);
 
-	const response = await projectController.createProject(req.prisma)(
-		name,
-		color
-	);
+    const response = await projectController.createProject(req.prisma)(
+        name,
+        color
+    );
 
-	if (response instanceof Error) {
-		if (
-			response.message.includes(
-				"Unique constraint failed on the fields: (`name`)"
-			)
-		) {
-			return res.status(StatusCode.ClientErrorConflict).json({
-				message: `A project named "${name}" already exists.`
-			});
-		}
+    if (response instanceof Error) {
+        if (
+            response.message.includes(
+                "Unique constraint failed on the fields: (`name`)"
+            )
+        ) {
+            return res.status(StatusCode.ClientErrorConflict).json({
+                message: `A project named "${name}" already exists.`
+            });
+        }
 
-		if (response.message.includes("Reached maximum amount of projects")) {
-			return res.status(StatusCode.ClientErrorConflict).json({
-				message: response.message
-			});
-		}
-	}
+        if (response.message.includes("Reached maximum amount of projects")) {
+            return res.status(StatusCode.ClientErrorConflict).json({
+                message: response.message
+            });
+        }
+    }
 
-	return utils.sanitizeResponse(
-		response,
-		res,
-		"Contact developers if this line appears. createProject request handler"
-	);
+    return utils.sanitizeResponse(
+        response,
+        res,
+        "Contact developers if this line appears. createProject request handler"
+    );
 };
 /**
  * @swagger
@@ -587,10 +586,10 @@ const createProject = async (req: Request, res: Response) => {
  */
 
 export {
-	getAllProjects,
-	createProject,
-	getProjectById,
-	getProjectByName,
-	updateProject,
-	deleteProjectById
+    getAllProjects,
+    createProject,
+    getProjectById,
+    getProjectByName,
+    updateProject,
+    deleteProjectById
 };
